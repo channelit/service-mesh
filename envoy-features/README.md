514557024fb5c912261d26128dc0d0b993f733b6
Hardik Patel <hardikpatel@Hardiks-MacBook-Pro.local>
2019-08-09 13:51:47 -0400

    jwt config

---
+++ envoy-features/README.md
@@ -0,0 +1,10 @@
+
+## URLs
+
+Services: http://localhost:8080
+Jaeger: http://localhost:16686
+Prometheus: http://localhost:9090
+Graphana: http://localhost:3001
+
+
+![Simple Sidecar Proxy](img/sidecar-proxy-simple.svg)
---
+++ envoy-features/locust/locust.py
@@ -0,0 +1,29 @@
+from locust import HttpLocust, TaskSet, task
+import os
+
+def index(l):
+    l.client.get("/")
+
+def stats(l):
+    l.client.get("/stats/requests")
+
+class UserTasks(TaskSet):
+    # one can specify tasks like this
+    tasks = [index, stats]
+    
+    # but it might be convenient to use the @task decorator
+    @task
+    def page404(self):
+        self.client.get("/does_not_exist")
+    
+class WebsiteUser(HttpLocust):
+    """
+    Locust user class that does requests to the locust web server running on localhost
+    """
+    URL_HOST = os.environ.get('URL_HOST', '127.0.0.1')
+    URL_PORT = os.environ.get('URL_PORT', '8089')
+    URL_ENDPOINT = os.environ.get('URL_ENDPOINT', 'client')
+    host = "http://" + URL_HOST + ":" + URL_PORT + "/" + URL_ENDPOINT
+    min_wait = 2000
+    max_wait = 5000
+    task_set =  UserTasks
