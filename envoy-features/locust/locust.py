1f99a88f5b3ea45bccd1f9fa39dc9a9a7483577d
Hardik <hardikgw@gmail.com>
2019-08-12 14:26:10 -0400

    locust updates

--- envoy-features/locust/locust.py
+++ envoy-features/locust/locust.py
@@ -1,5 +1,6 @@
 from locust import HttpLocust, TaskSet, task
 import os
+import json
 
 def index(l):
     l.client.get("/")
@@ -7,19 +8,43 @@ def index(l):
 def stats(l):
     l.client.get("/stats/requests")
 
+class MultipleHostsLocust(HttpLocust):
+    abstract = True
+    
+    def __init__(self, *args, **kwargs):
+        super(MultipleHostsLocust, self).__init__(*args, **kwargs)
+        self.jwt_client = HttpSession(base_url=os.environ.get("JWT_URL", "http://localhost:8080"))
+
 class UserTasks(TaskSet):
-    # one can specify tasks like this
+
     tasks = [index, stats]
     
-    # but it might be convenient to use the @task decorator
+    def on_start(self):
+        self.login()
+
+    def login(self)
+        conn = http.client.HTTPConnection("localhost")
+        payload = "username=user&password=user&grant_type=password&client_id=envoy"
+        headers = {
+            'Content-Type': "application/x-www-form-urlencoded",
+            'User-Agent': "PostmanRuntime/7.15.2",
+            'Accept': "*/*",
+            'Cache-Control': "no-cache",
+            'Host': "localhost:8080",
+            'Accept-Encoding': "gzip, deflate",
+            'Content-Length': "63",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+            }
+        response = self.jwt_client.post('/auth/realms/envoy/protocol/openid-connect/token',
+                         payload, 
+                         headers=headers)
+
     @task
     def page404(self):
         self.client.get("/does_not_exist")
     
-class WebsiteUser(HttpLocust):
-    """
-    Locust user class that does requests to the locust web server running on localhost
-    """
+class WebsiteUser(MultipleHostsLocust):
     URL_HOST = os.environ.get('URL_HOST', '127.0.0.1')
     URL_PORT = os.environ.get('URL_PORT', '8089')
     URL_ENDPOINT = os.environ.get('URL_ENDPOINT', 'client')
