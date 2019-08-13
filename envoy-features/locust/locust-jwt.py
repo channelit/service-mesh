26e482c925bdc8a8743174971f529a4adf8408bd
Hardik <hardikgw@gmail.com>
2019-08-12 23:09:09 -0400

    locust jwt

--- envoy-features/locust/locust-jwt.py
+++ envoy-features/locust/locust-jwt.py
@@ -1,29 +1,23 @@
 from locust import HttpLocust, TaskSet, task
+from locust.clients import HttpSession
 import os
 import json
 
 def index(l):
     l.client.get("/")
 
+
 def stats(l):
     l.client.get("/stats/requests")
 
-class MultipleHostsLocust(HttpLocust):
-    abstract = True
-    
-    def __init__(self, *args, **kwargs):
-        super(MultipleHostsLocust, self).__init__(*args, **kwargs)
-        self.jwt_client = HttpSession(base_url=os.environ.get("JWT_URL", "http://localhost:8080"))
 
 class UserTasks(TaskSet):
-
     tasks = [index, stats]
-    
+
     def on_start(self):
         self.login()
 
-    def login(self)
-        conn = http.client.HTTPConnection("localhost")
+    def login(self):
         payload = "username=user&password=user&grant_type=password&client_id=envoy"
         headers = {
             'Content-Type': "application/x-www-form-urlencoded",
@@ -35,20 +29,28 @@ class UserTasks(TaskSet):
             'Content-Length': "63",
             'Connection': "keep-alive",
             'cache-control': "no-cache"
-            }
-        response = self.jwt_client.post('/auth/realms/envoy/protocol/openid-connect/token',
-                         payload, 
-                         headers=headers)
+        }
+        client = HttpSession(base_url=os.environ.get("JWT_URL", "http://localhost:8080"))
+        response = client.post('/auth/realms/envoy/protocol/openid-connect/token',
+                                        payload,
+                                        headers=headers)
+        auth_token = json.loads(response.text)['access_token']
+        print(auth_token)
 
     @task
     def page404(self):
         self.client.get("/does_not_exist")
-    
-class WebsiteUser(MultipleHostsLocust):
+
+
+class WebsiteUser(HttpLocust):
     URL_HOST = os.environ.get('URL_HOST', '127.0.0.1')
-    URL_PORT = os.environ.get('URL_PORT', '8089')
+    URL_PORT = os.environ.get('URL_PORT', '8090')
     URL_ENDPOINT = os.environ.get('URL_ENDPOINT', 'client')
     host = "http://" + URL_HOST + ":" + URL_PORT + "/" + URL_ENDPOINT
     min_wait = 2000
     max_wait = 5000
-    task_set =  UserTasks
+    task_set = UserTasks
+
+
+if __name__ == '__main__':
+    WebsiteUser().run()
