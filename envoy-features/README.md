80f747a6eb74ce7083b5444dc4559ec4e6ff9456
Hardik <hardikgw@gmail.com>
2019-08-15 15:40:48 -0400

    readme and istio configus

--- envoy-features/README.md
+++ envoy-features/README.md
@@ -8,9 +8,19 @@ Services: http://localhost:8080
 Jaeger: http://localhost:16686
 Prometheus: http://localhost:9090
 Graphana: http://localhost:3001
+Keycloak: http://localhost:8080
+Locsust: http://localhost:8089/
 
 ## Run Locust locally (won't work unless mapped localhost)
 
 docker run --name=locust -v $(pwd)/locust-jwt.py:/locust.py -p 8421:8089 -e URL_HOST=localhost -e URL_PORT=8090 -e JWT_URL=http://localhost:8080 --network="host" cithub/locust locust -f /locust.py
 
 ![Simple Sidecar Proxy](img/sidecar-proxy-simple.svg)
+
+## Steps:
+
+1. docker-compose up
+2. Create user in Keycloak
+3. Validate user in Postman
+4. Run locust for 200 users
+5. Add (or remove if not already there) circuit breaker configuration in front_svc and repeat steps 1-4
---
+++ istio-kube-features/volume/locust/locust-jwt.py
@@ -0,0 +1,49 @@
+from locust import HttpLocust, TaskSet, task
+from locust.clients import HttpSession
+import os
+import json
+
+
+class SubscriberTasks(TaskSet):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.auth_token = ""
+
+    def on_start(self):
+        self.login()
+
+    def login(self):
+        payload = "username=user&password=user&grant_type=password&client_id=envoy"
+        headers = {
+            'Content-Type': "application/x-www-form-urlencoded",
+        }
+        client = HttpSession(base_url=os.environ.get("JWT_URL", "http://localhost:8080"))
+        response = client.post('/auth/realms/envoy/protocol/openid-connect/token',
+                               payload,
+                               headers=headers)
+        self.auth_token = "Bearer " + json.loads(response.text)['access_token']
+
+    @task(1)
+    def call_client(self):
+        response = self.client.get("client", headers={'Authorization': self.auth_token})
+        print(self.auth_token)
+        print(response.text)
+
+    @task(2)
+    def call_status(self):
+        self.client.get("status", headers={'Authorization': self.auth_token})
+
+
+class ServiceUser(HttpLocust):
+    URL_HOST = os.environ.get('URL_HOST', '127.0.0.1')
+    URL_PORT = os.environ.get('URL_PORT', '8090')
+    URL_ENDPOINT = os.environ.get('URL_ENDPOINT', '')
+    host = "http://" + URL_HOST + ":" + URL_PORT + "/" + URL_ENDPOINT
+    min_wait = 2000
+    max_wait = 5000
+    task_set = SubscriberTasks
+
+
+if __name__ == '__main__':
+    ServiceUser().run()
